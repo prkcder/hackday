cb45323b9c31d29b4a9ce6830199d28a998af859
Mario Menjivar <mariomenjr@gmail.com>
2019-10-26 18:23:25 -0700

    Implemented Endpoints

--- public/main.js
+++ public/main.js
@@ -1,95 +1,12 @@
-const dictionary = {
-    dna: {
-        binary: {
-            '01': 'T',
-            '10': 'G',
-            '00': 'A',
-            '11': 'C'
-        },
-        letters: {
-            'T': '01',
-            'G': '10',
-            'A': '00',
-            'C': '11'
-        }
-    },
-    rna: {
-        binary: {
-            '01': 'U',
-            '10': 'G',
-            '00': 'A',
-            '11': 'C'
-        },
-        letters: {
-            'U': '01',
-            'G': '10',
-            'A': '00',
-            'C': '11'
-        }
-    }
-};
-/*
-function Dna(str) {
-    const chars = str.split("");
-    const bins = chars.map(char => getEightCharacterString(char.charCodeAt(0).toString(2)));
-    return bins.map(
-        word => {
-            const pairs = word.match(/.{1,2}/g);
-            return pairs.map(
-                pair => {
-                    return dictionary.dna.binary[pair];
-                }
-            ).join("")
-        }
-    ).join("");
-}
-*/
-// flag: 0 := dna encode; 1:= rna
-// function encode(str, flag) {
-//     const chars = str.split("");
-//     const bins = chars.map(char => getEightCharacterString(char.charCodeAt(0).toString(2)));
-//     return bins.map(
-//         word => {
-//             const pairs = word.match(/.{1,2}/g);
-//             return pairs.map(
-//                 pair => {
-                    // return dictionary[flag ? 'rna' : 'dna'].binary[pair];
-//                 }
-//             ).join("")
-//         }
-//     ).join("");
-// }
-
-function encode(str, flag) {
-    const chars = str.split("");
-    const bins = chars.map(char => getEightCharacterString(char.charCodeAt(0).toString(2)));
-    return bins.map(
-        word => {
-            const pairs = word.match(/.{1,2}/g);
-            return pairs.map(
-                pair => {
-                    var encoder;
-                    if (flag == 0) encoder = 'dna';
-                    else if (flag == 1) encoder = 'rna';
-                    return dictionary[encoder].binary[pair];
-                }
-            ).join("");
-        }
-    ).join("")
-}
-
-function getEightCharacterString(inputString) {
-    var zeroes = "";
-    while (inputString.length + zeroes.length < 8) {
-        zeroes += "0";
-    }
-    zeroes += inputString
-    return zeroes
-}
-
 function dnaButtonClicked() {
-    var inputVal = document.getElementById("input").value
-    dnaValue = encode(inputVal, 1)
-    document.getElementById("result").value = dnaValue
+  var inputVal = document.getElementById("input").value;
+
+  axios
+    .get(
+      "http://localhost:3000/api/rna/encode/mariom"
+    )
+    .then(res => {
+        document.getElementById("result").value = res.data.resp;
+    });
+  
 }
-// console.log(encode("cat", 1));
--- server/server.js
+++ server/server.js
@@ -1,13 +1,47 @@
 const express = require('express');
+const cors = require('cors')
+
+const { Dna, Rna } = require('../encoder');
 
 const app = express();
 
+app.use(cors())
 app.use(express.static('dist'));
 app.use(express.static('public'));
 
-app.get('/a',(req, res) => {
-    res.send('slash route works')
+// DNA
+
+app.get('/api/dna/encode/:str',(req, res) => {
+    const { str } = req.params;
+
+    const encd = new Dna();
+    const resp = encd.encode(str);
+
+    res.json({ resp });
 });
+
+app.get('/api/dna/valid-substr/:str',(req, res) => {
+    const { str } = req.params;
+
+    const encd = new Dna();
+    const resp = encd.isDna(str);
+
+    res.json({ resp });
+});
+
+
+
+// RNA
+
+app.get('/api/rna/encode/:str',(req, res) => {
+    const { str } = req.params;
+
+    const encd = new Rna();
+    const resp = encd.encode(str);
+
+    res.json({ resp });
+});
+
 
 
 module.exports = app;
