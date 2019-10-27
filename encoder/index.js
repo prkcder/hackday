ffd88b1c7bf7c07d86a06fd4c8f0093a06171c66
Mario Menjivar <mariomenjr@gmail.com>
2019-10-26 17:56:34 -0700

    Completed Objective 5 & 6

--- encoder/index.js
+++ encoder/index.js
@@ -1,6 +1,6 @@
 const dictionary = require("./config");
 
-function getEightCharacterString(inputString) {
+function _getEightCharacterString(inputString) {
     var zeroes = "";
     while (inputString.length + zeroes.length < 8) {
         zeroes += "0";
@@ -9,9 +9,15 @@ function getEightCharacterString(inputString) {
     return zeroes
 }
 
-function encondeAs(str, type) {
+function _isValidEncoding(str, type) {
+    const valid = dictionary[type].letters;
+
+    return str.split("").every(char => valid.hasOwnProperty(char));
+}
+
+function _encondeAs(str, type) {
     const chars = str.split("");
-    const bins = chars.map(char => getEightCharacterString(char.charCodeAt(0).toString(2)));
+    const bins = chars.map(char => _getEightCharacterString(char.charCodeAt(0).toString(2)));
 
     return bins.map(
         word => {
@@ -26,16 +32,46 @@ function encondeAs(str, type) {
     ).join("");
 }
 
+function _decodeAs(str, type, isComplementary) {
+    if (!_isValidEncoding(str, 'dna')) return "";
+
+    const fours = str.match(/.{1,4}/g);
+
+    return fours.map(four => {
+        const word = four.split("").map(char => {
+
+            if (isComplementary) return dictionary[type].complementary.letters[char];
+
+            return dictionary[type].letters[char];
+        }).join("");
+        return String.fromCharCode(parseInt(word, 2));
+    });
+}
+
 function Dna() {}
 
 Dna.prototype.encode = function(str) {
-    return encondeAs(str, 'dna');
+    return _encondeAs(str, 'dna');
+};
+
+Dna.prototype.isDna = function(str) {
+    return _isValidEncoding(str, 'dna');
+};
+
+Dna.prototype.decode = function(str) {
+    return _decodeAs(str, 'dna');
+};
+
+Dna.prototype.decodeComplementary = function(str) {
+    return _decodeAs(str, 'dna', true);
 };
 
 function Rna() {}
 
 Rna.prototype.encode = function(str) {
-    return encondeAs(str, 'rna');
+    return _encondeAs(str, 'rna');
 };
+
+const sample = new Dna().decodeComplementary('ACTGACTAAGAT');
 
 module.exports = { Dna, Rna };
