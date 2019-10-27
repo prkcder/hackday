99091632ac6dcc790a2bbe9b49487b56d3dc70ae
Jackson Tubbs <decknot@ucr-guest-01-10-14-1-10.wnet.ucr.edu>
2019-10-26 18:31:16 -0700

    Adds rna

--- public/main.js
+++ public/main.js
@@ -1,12 +1,23 @@
 function dnaButtonClicked() {
   var inputVal = document.getElementById("input").value;
-
+  console.log(inputVal);
   axios
     .get(
-      "http://localhost:3000/api/rna/encode/mariom"
+      "http://localhost:3000/api/dna/encode/" + inputVal
     )
     .then(res => {
         document.getElementById("result").value = res.data.resp;
     });
-  
+}
+
+function rnaButtonClicked() {
+  var inputVal = document.getElementById("input").value;
+  console.log(inputVal);
+  axios
+    .get(
+      "http://localhost:3000/api/rna/encode/" + inputVal
+    )
+    .then(res => {
+        document.getElementById("result").value = res.data.resp;
+    });
 }
